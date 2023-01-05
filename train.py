5532b72893d0481e3ac5c2af6ae3f1aaef4adc89
Krishna Thoriya <70200639+Krishna0312@users.noreply.github.com>
2023-01-06 01:26:10 +0530

    Update train.py

--- train.py
+++ train.py
@@ -11,7 +11,7 @@ from keras import backend as K
 
 from data import load_train_data, load_test_data
 
-K.set_image_dim_ordering('th')  # Theano dimension ordering in this code
+K.common.set_image_dim_ordering('th')  # Theano dimension ordering in this code  .common added
 
 img_rows = 100
 img_cols = 160
