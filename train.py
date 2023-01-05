d43a74f33ea076a08d55ab440de811859c6bf423
Krishna Thoriya <70200639+Krishna0312@users.noreply.github.com>
2023-01-06 01:18:48 +0530

    Update train.py

--- train.py
+++ train.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 from keras.models import Model
-from keras.layers.advanced_activations import LeakyReLU
+from keras.layers import LeakyReLU
 from keras.layers import Input, merge, SpatialDropout2D
 from keras.layers import Convolution2D, AveragePooling2D, UpSampling2D
 from keras.optimizers import Adam
